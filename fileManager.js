f93277264934c4da874c03a634c543263ad00e95
Ne0n2018 <beevertm@gmail.com>
2025-03-18 09:20:35 +0300

    complited

--- fileManager.js
+++ fileManager.js
@@ -2,6 +2,10 @@ import readline from "readline";
 import os from "os";
 import { changeDirectory } from "./commands/changeDirectory.js";
 import { listDirectory } from "./commands/listDirectory.js";
+import { fileOperations } from "./commands/fileOperations.js";
+import { getOsInfo } from "./commands/osInfo.js";
+import { hashFile } from "./commands/hashCalculator.js";
+import { compressFile, decompressFile } from "./commands/compression.js";
 
 let currentDirectory = os.homedir();
 
@@ -24,6 +28,27 @@ export function startFileManager(username) {
             break;
           case "ls":
             await listDirectory(currentDirectory);
+            break;
+          case "cp":
+            fileOperations("copy", args, currentDirectory);
+            break;
+          case "rn":
+            fileOperations("rename", args, currentDirectory);
+            break;
+          case "rm":
+            fileOperations("delete", args, currentDirectory);
+            break;
+          case "os":
+            getOsInfo(args[0]);
+            break;
+          case "hash":
+            await hashFile(args[0], currentDirectory);
+            break;
+          case "compress":
+            await compressFile(args[0], args[1], currentDirectory);
+            break;
+          case "decompress":
+            await decompressFile(args[0], args[1], currentDirectory);
             break;
           case ".exit":
             console.log(
